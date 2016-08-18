209245e51b73fa1a5eafb67431580ddd8564c5a2
ibazzi <ibazzi@qq.com>
2016-08-18 20:55:33 +0800

    fixed cross compile

---
+++ n2n_v2/cmake/CMakeToolchainFile.mips.cmake
@@ -0,0 +1,6 @@
+# the name of the target operating system
+SET(CMAKE_SYSTEM_NAME Linux)
+
+# which compilers to use for C and C++
+SET(CMAKE_C_COMPILER mips-linux-gcc)
+SET(CMAKE_CXX_COMPILER mips-linux-g++)
---
+++ n2n_v2/cmake/CMakeToolchainFile.mipsel.cmake
@@ -0,0 +1,6 @@
+# the name of the target operating system
+SET(CMAKE_SYSTEM_NAME Linux)
+
+# which compilers to use for C and C++
+SET(CMAKE_C_COMPILER mipsel-linux-gcc)
+SET(CMAKE_CXX_COMPILER mipsel-linux-g++)
